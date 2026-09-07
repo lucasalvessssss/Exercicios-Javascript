4bb4ce1a71d220a2b448c6e7f26d21b6e338de83
Lucas Alves <luc.antonio.lj@gmail.com>
2026-09-07 17:13:00 -0300

    Aula-05(Declarando variáveis)

---
+++ Aulas/Aula-05/index.js
@@ -0,0 +1,13 @@
+//----ATIVIDADE DECLARAÇÃO DE VARIÁVEIS----//
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não podemos começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+
+let nome = 'João'; // String
+let idade = 25;
+
+console.log(nome);
+console.log(idade);
